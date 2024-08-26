4d4b214ed62df6dd87d06daadbb8e5f2a0efb747
Thomas Brears <thomasbrears123@gmail.com>
2024-08-27 11:12:31 +1200

    readding cors and port

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { connectToDb } from './db.js';
 import productRoutes from './routes/products.js';
 import retailerRoutes from './routes/retailers.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+// Cores middleware to allow cross-origin requests
+app.use(cors());
 
 // Use the routes
 app.use('/api/products', productRoutes);
@@ -14,7 +19,7 @@ app.use('/api/retailers', retailerRoutes);
 
 // Connect to the database and start the server
 connectToDb(() => {
-    app.listen(8000, () => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log('Server is running on port 8000');
     });
 });
