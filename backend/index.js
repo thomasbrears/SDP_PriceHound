08017b8590e9d0ed0990ef8901dd8b65918cf9db
Thomas Brears <thomasbrears123@gmail.com>
2024-09-03 17:09:45 +1200

    Change from MongoDB to FireBase DB

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import { connectToDb } from './db.js';
 import productRoutes from './routes/products.js';
 import retailerRoutes from './routes/retailers.js';
-import userRoutes from './routes/users.js'
+import userRoutes from './routes/users.js';
 import dotenv from 'dotenv';
+import { db } from './firebase.js';
+
 dotenv.config();
 
 const app = express();
@@ -32,12 +33,11 @@ app.use((err, req, res, next) => {
 app.use('/api/products', productRoutes);
 app.use('/api/retailers', retailerRoutes);
 app.use('/api/userinfo', userRoutes);
+
 // Use environment variable PORT, or default to 8000
 const PORT = process.env.PORT || 8000;
 
-// Connect to the database and start the server
-connectToDb(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log('Server is running on port 8000');
-    });
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
