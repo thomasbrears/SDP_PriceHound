5a28b3047837332e1944d3ebb91b7a5a0d6a024f
Thomas Brears <thomasbrears123@gmail.com>
2024-09-11 11:32:16 +1200

    Add account delete function and adjusted manage account style

--- frontend/src/pages/ManageAccountPage.jsx
+++ frontend/src/pages/ManageAccountPage.jsx
@@ -1,16 +1,19 @@
-
 import React, { useState, useEffect } from 'react';
-import { getAuth, updateProfile, updateEmail, updatePassword, reauthenticateWithCredential, EmailAuthProvider } from 'firebase/auth';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { getAuth, onAuthStateChanged, updateProfile, updateEmail, updatePassword, reauthenticateWithCredential, EmailAuthProvider } from 'firebase/auth';
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage';
+import { storage } from '../FirebaseAuth/Firebase';
+import { useNavigate } from 'react-router-dom';
 import MainHeadTitle from '../components/MainHeadTitle';
 import PinkButton from '../components/PinkButton';
+import Message from '../components/Message';
 import '../css/PinkButton.css';
 import '../css/ManageAccountPage.css';
-import { storage } from '../FirebaseAuth/Firebase';
-import Message from '../components/Message';
+
 
 function ManageAccountPage() {
   const auth = getAuth();
+  const navigate = useNavigate();
+
   // state for current user data
   const [currentUser, setCurrentUser] = useState({
     name: '',
@@ -27,30 +30,45 @@ function ManageAccountPage() {
   const [newName, setnewName] = useState('');
   const [icon, setIcon] = useState(null);
   const [pfp, setPfp] = useState(null);
+  const [passwordConfirmation, setPasswordConfirmation] = useState(''); // Password to confirm deletion
   //const [newEmail, setNewEmail] = useState('');
+
   //  fetch user data on component mount
   useEffect(() => {
-    const url = localStorage.getItem('icon');
-    setPfp(url)
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-    if (storedUser) {
-      setCurrentUser({
-        name: storedUser.displayName || storedUser.email || 'No Name', // Or default name if displayName is not set
-        email: storedUser.email,
-        uid: storedUser.uid,
-      });
-    }
-  }, []);
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, update user state
+        setCurrentUser({
+          name: user.displayName || 'No Name',
+          email: user.email,
+          uid: user.uid
+        });
+        localStorage.setItem('user', JSON.stringify({
+          displayName: user.displayName,
+          email: user.email,
+          uid: user.uid
+        }));
+      } else {
+        // User is signed out or account deleted
+        localStorage.clear(); // Clear any stored user data
+        navigate('/login'); // Redirect to login page or homepage
+      }
+    });
+  
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [navigate]); // 'navigate' as a dependency  
 
   const handleChangePassword = async (e) => {
     e.preventDefault();
 
     if (newPassword !== confirmNewPassword) {
-      alert('New passwords do not match!');
+      setMessageInfo({ message: 'New passwords do not match!', type: 'error' });
       return;
     }
     if (newPassword.length < 6) {
-      alert('Password must be at least 6 characters long!');
+      setMessageInfo({ message: 'Password must be at least 6 characters long!', type: 'error' });
       return;
     }
 
@@ -60,35 +78,54 @@ function ManageAccountPage() {
       const credential = EmailAuthProvider.credential(user.email, currentPassword);
       await reauthenticateWithCredential(user, credential);
       await updatePassword(user, newPassword);
-      alert('Password updated successfully');
+      setMessageInfo({ message: 'Password updated successfully', type: 'success' });
     } catch (error) {
       console.error('Error updating password:', error);
-      alert('Error updating password');
+      setMessageInfo({ message: 'Error updating password', type: 'error' });
     }
   };
+
   const handleChangeName = async (e) => {
     e.preventDefault();
 
     if (newName.length < 1) {
-      alert('Please enter a name!');
+      setMessageInfo({ message: 'Please enter a name!', type: 'error' });
       return;
     }
 
     try {
       const user = auth.currentUser;
       await updateProfile(user, { displayName: newName });
+      await user.reload(); // Reload user data from Firebase
+
       setCurrentUser({ ...currentUser, name: newName });
       localStorage.setItem('user', JSON.stringify({ ...currentUser, name: newName }));
-      alert('Name updated successfully');
+
+      setMessageInfo({ message: 'Name updated successfully', type: 'success' });
     } catch (error) {
       console.error('Error updating name:', error);
-      alert('Error updating name');
+      setMessageInfo({ message: 'Error updating name; Please try again', type: 'error' });
     }
   };
+
+
   const handleFileChange = (e) => {
-    setIcon(e.target.files[0]); // Set the selected file to the state
-    //https post goes here.
+    const file = e.target.files[0];
+    
+    // List of acceptable file types
+    const acceptableTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp', 'image/svg+xml', 'image/heic', 'image/heif'];
+  
+    if (file && !acceptableTypes.includes(file.type)) {
+      // Set an error message if the file type is not acceptable
+      setMessageInfo({ message: 'File type not accepted. Please upload a valid image file (JPEG, JPG, PNG, WEBP, SVG, or HEIC).', type: 'error' });
+      return;
+    }
+    
+    // Clear previous messages and proceed with file handling if valid
+    setMessageInfo({ message: '', type: '' });
+    setIcon(file); // Set the selected file to the state
   };
+  
 
   const fetchIcon = async (uid) => {
     try {
@@ -103,7 +140,7 @@ function ManageAccountPage() {
 
   const handleChangeIcon = async (e) => {
     if (!icon) {
-      alert('Please select an icon file!');
+      setMessageInfo({ message: 'Please select an image file!', type: 'error' });
       return;
     }
     try {
@@ -112,8 +149,48 @@ function ManageAccountPage() {
       await uploadBytes(storageRef, icon, {
         contentType: icon.type,
       });
-
+      setMessageInfo({ message: 'Icon updated successfully', type: 'success' });
     } catch (error) {
+      console.error('Error updating icon:', error);
+      setMessageInfo({ message: 'Error updating icon', type: 'error' });
+    }
+  };
+
+  // Handle account deletion
+  const handleDeleteAccount = async () => {
+    if (!window.confirm('Are you sure you want to permanently delete your account? THIS ACTION CANOT BE UNDONE!')) {
+      return;
+    }
+
+    try {
+      const user = auth.currentUser;
+      const credential = EmailAuthProvider.credential(user.email, passwordConfirmation); // Reauthenticate with the password
+      await reauthenticateWithCredential(user, credential);
+      await deleteUserData(user.uid); // Delete user data
+      await user.delete(); // Delete user account from Firebase Auth
+      setMessageInfo({ message: 'Your account has been deleted successfully; Have a good day', type: 'success' });
+            
+      // Delay to show success message
+      setTimeout(() => {
+        auth.signOut(); // Sign out the user
+        localStorage.clear(); // Clear localStorage
+        navigate('/'); // Redirect to the homepage
+        window.location.reload(); 
+      }, 4000); // 4-second delay
+    } catch (error) {
+      console.error('Error deleting account:', error);
+      setMessageInfo({ message: 'Error deleting account. Please try again.', type: 'error' });
+    }
+  };
+
+  // Function to delete related user data (e.g., Firestore, storage)
+  const deleteUserData = async (uid) => {
+    try {
+      // Example: Delete user-related data from Firestore, Storage, etc.
+      const storageRef = ref(storage, `icons/${uid}`);
+      await deleteObject(storageRef); // Delete profile picture from storage
+    } catch (error) {
+      console.error('Error deleting user data:', error);
     }
   };
 
@@ -157,29 +234,29 @@ function ManageAccountPage() {
         <div className='change-password'>
           <h1>Change Password</h1>
           <form onSubmit={handleChangePassword} className='change-Form'>
-            <label>Current Password</label>
+            <label>Please enter your Current Password</label>
             <input
               type='password'
               onChange={(e) => setCurrentPassword(e.target.value)}
               required
               className="formInput"
-              placeholder='Enter your current password'
+              placeholder='************'
             />
-            <label>New Password</label>
+            <label>Please enter your New Password</label>
             <input
               type='password'
               onChange={(e) => setNewPassword(e.target.value)}
               required
               className="formInput"
-              placeholder='Enter your new password'
+              placeholder='************'
             />
-            <label>Confirm New Password</label>
+            <label>Please Confirm your New Password</label>
             <input
               type='password'
               onChange={(e) => setConfirmNewPassword(e.target.value)}
               required git
               className="formInput"
-              placeholder='Confirm your new password'
+              placeholder='************'
             />
 
             <PinkButton text="Change Password" />
@@ -188,15 +265,16 @@ function ManageAccountPage() {
         </div>
         <div className='change-name'>
           <h1>Change Name </h1>
-          <p>Current name is {currentUser.name}</p>
+          <p>Currently your name is set to, {currentUser.name}</p>
+          <br />
           <form onSubmit={handleChangeName} className='change-Form'>
-            <label >Enter New Name</label>
+            <label >Please enter a new name</label> 
             <input
               type='name'
               onChange={(e) => setnewName(e.target.value)}
               required
               className="formInput"
-              placeholder='Enter your new name'
+              placeholder='e.g. John Doe'
             />
 
             <PinkButton text="Change Name" />
@@ -204,28 +282,41 @@ function ManageAccountPage() {
           </form>
         </div>
         <div className='change-icon'>
-          <h1>Change Icon</h1>
+          <h1>Change Profile Picture</h1>
           <form onSubmit={handleChangeIcon} className='change-Form'>
-            <label>Select New Icon</label>
+            <label>Please select New Profile Picture</label>
             <input
               type='file'
-              accept='image/*'
+              accept='.jpg, .jpeg, .png, .webp, .svg, .heic, .heif' // Specify accepted image file types
               onChange={handleFileChange}
               required
               className="formInput"
             />
-            <PinkButton text="Change Icon" />
+            <PinkButton text="Change Picture" />
           </form>
         </div>
+
         <div className='change-email'>
           <h1>Change Email</h1>
-          <h3>Your current email address is {currentUser.email} </h3>
-          <p>To change this, please contact us.</p>
+          <h4>Your current email address is {currentUser.email} </h4>
+          <p>To change this, please contact us</p>
+          <br />
+          <PinkButton text="Contact us" to="/contact" />
         </div>
+
         <div className='delete-account'>
           <h1>Delete Account</h1>
-          <p>If you would like to permanently delete your account , please contact us.</p>
+          <p>To permanently delete your account, please authenticate by entering your password below. <br /> <b>This action is irreversible</b></p>
+          <input
+            type='password'
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+            required
+            className="formInput"
+            placeholder='*************'
+          />
+          <PinkButton text="Delete Account" onClick={handleDeleteAccount} />
         </div>
+
       </div>
       {messageInfo.message && <Message message={messageInfo.message} type={messageInfo.type} />}
     </div>
